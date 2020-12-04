66715a16b82c2d7e4c2e79f62bce9bab6b7d7aee
Filip Johansson <filip.johansson@protonmail.com>
2020-12-04 10:24:07 +0200

    more updates

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -9,34 +9,35 @@ const Wrapper = styled.div`
   border-top-left-radius: 6px;
   border-top-right-radius: 6px;
   margin-right: 5px;
-    ${(props) => props.selected &&
-      css`
-        background-color: #222;
-        border-top: 2px solid #222;
-        border-left: 2px solid #222;
-        border-right: 2px solid #222;
-        color: #fff;
-      `
-    }
-    ${(props) => !props.selected &&
-      css`
-        border-top: 2px dashed #222;
-        border-left: 2px dashed #222;
-        border-right: 2px dashed #222;
-        color: #222;
-      `
-    }
+  ${(props) =>
+    props.selected &&
+    css`
+      background-color: #222;
+      border-top: 2px solid #222;
+      border-left: 2px solid #222;
+      border-right: 2px solid #222;
+      color: #fff;
+    `}
+  ${(props) =>
+    !props.selected &&
+    css`
+      border-top: 2px dashed #222;
+      border-left: 2px dashed #222;
+      border-right: 2px dashed #222;
+      color: #222;
+    `}
 `;
 
 const Tab = ({ state, dispatch, endpoint, mappingIndex }) => {
   console.log(mappingIndex === state.active);
 
   return (
-    <Wrapper onClick={defer(dispatch, {type: 'SET_ACTIVE', payload: mappingIndex})}
+    <Wrapper
+      onClick={defer(dispatch, { type: "SET_ACTIVE", payload: mappingIndex })}
       selected={mappingIndex === state.active}
     >
       <Input
-      color="#fff"
+        color="#fff"
         defaultValue={endpoint}
         onChange={eventAction(dispatch, "SET_ENDPOINT")}
       />
